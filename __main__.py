440a78d1d78f94ffa2a3e7312615739193cc2bed
kovacsalex <kovacsalex95@gmail.com>
2021-01-11 10:12:10 +0100

    Initial commit

--- __main__.py
+++ __main__.py
@@ -1,11 +1,63 @@
-import sys
+def is_number(num):
+    return num.isnumeric()
 
-def load_module(module):
-    module_path = module
+def is_valid_operator(operator):
+    if (operator == '+'):
+        return True
+    if (operator == '-'):
+        return True
+    if (operator == '*'):
+        return True
+    if (operator == '/'):
+        return True
+    return False
 
-    if module_path in sys.modules:
-        return sys.modules[module_path]
+def ask_for_a_number(force_valid_input = False):
+    inp = input('Please provide a number!')
 
-    return __import__(module_path, fromlist=[module])
+    if (force_valid_input):
+        while is_number(inp) == False:
+            inp = input('Please provide a number!')
+    elif (is_number(inp) == False):
+        inp = None
 
-print('Hello world')
+    return inp;
+
+def ask_for_an_operator(force_valid_input = False):
+    inp = input('Please provide an operator (one of +, -, *, /)!')
+
+    if (force_valid_input):
+        while is_valid_operator(inp) == False:
+            inp = input('Please provide an operator (one of +, -, *, /)!')
+    elif (is_valid_operator(inp) == False):
+        inp = None
+
+    return inp;
+
+def calc(operator, num1, num2):
+    if (operator == '+'):
+        return float(num1) + float(num2)
+    if (operator == '-'):
+        return float(num1) - float(num2)
+    if (operator == '*'):
+        return float(num1) * float(num2)
+    if (operator == '/' and num2 == '0'):
+        return None
+    if (operator == '/'):
+        return float(num1) / float(num2)
+    return None
+
+def simple_calculator():
+    num1 = ask_for_a_number(True)
+    op = ask_for_an_operator(True)
+    num2 = ask_for_a_number(True)
+
+    result = calc(op, num1, num2)
+
+    if (result != None):
+        print(result)
+
+    else:
+        print('You should not divide something with zero :(')
+
+simple_calculator()
